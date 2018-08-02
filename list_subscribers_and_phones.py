f011a4626486856f3846eb0eff5dff298412ffab
David Staudt <dstaudt@cisco.com>
2018-08-02 16:11:00 -0500

    Update source copyright notices to match license

--- list_subscribers_and_phones.py
+++ list_subscribers_and_phones.py
@@ -7,7 +7,7 @@ Script Dependencies:
 Depencency Installation:
     $ pip install suds-jurko
 
-Copyright (c) 2018 Cisco and/or its affiliates.
+Copyright (c) 2018 Cisco DevNet
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
